0d6dd46325971ceaeb1f0076e46c87ecfb8f48fd
dribnif <hazica@gmail.com>
2018-08-26 23:38:30 +0200

    Clean up some leftover tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,3 @@
 import locale
-from unittest import TestCase
-from ti import *
 
 locale.getdefaultlocale()
-
-# class TestMainClass(TestCase):
-#
-#     def test_is_working(self):
-#         is_working()
